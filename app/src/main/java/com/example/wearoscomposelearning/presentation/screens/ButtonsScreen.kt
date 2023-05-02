35fc9a8101d0a19c1ad0839b791a67253828716b
Seif Mohamed <seifeldinmohamed101@gmail.com>
2023-05-02 17:09:35 +0300

    WelcomeScreen.kt and ButtonsScreen.kt Added

---
+++ app/src/main/java/com/example/wearoscomposelearning/presentation/screens/ButtonsScreen.kt
@@ -0,0 +1,52 @@
+package com.example.wearoscomposelearning.presentation.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
+import androidx.wear.compose.material.Icon
+import com.example.wearoscomposelearning.presentation.theme.WearOSComposeLearningTheme
+
+@Composable
+fun ButtonsScreen() {
+    WearOSComposeLearningTheme {
+        // Modifiers used by our Wear composables.
+        val contentModifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp)
+        val iconModifier = Modifier
+            .size(24.dp)
+            .wrapContentSize(align = Alignment.Center)
+
+        ButtonExample(contentModifier, iconModifier)
+    }
+
+}
+
+@Composable
+fun ButtonExample(
+    modifier: Modifier = Modifier,
+    iconModifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            modifier = Modifier.size(ButtonDefaults.LargeButtonSize),
+            onClick = { /* ... */ },
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Phone,
+                contentDescription = "triggers phone action",
+                modifier = iconModifier
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/wearoscomposelearning/presentation/screens/WelcomeScreen.kt
@@ -0,0 +1,39 @@
+package com.example.wearoscomposelearning.presentation.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import com.example.wearoscomposelearning.R
+import com.example.wearoscomposelearning.presentation.theme.WearOSComposeLearningTheme
+
+@Composable
+fun WelcomeScreen(greetingName: String) {
+    WearOSComposeLearningTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Greeting(greetingName = greetingName)
+        }
+    }
+}
+
+@Composable
+fun Greeting(greetingName: String) {
+    Text(
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colors.primary,
+        text = stringResource(R.string.hello_world, greetingName)
+    )
+}
